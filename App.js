948b02cc6ef60184091291ee2c38a5496922cafe
贾元发 <jiayuanfa@yeah.net>
2021-12-31 13:24:22 +0800

    引入钱包链接模块

--- App.js
+++ App.js
@@ -8,6 +8,11 @@
 
 import React from 'react';
 import type {Node} from 'react';
+
+import WalletConnectProvider, { 
+  useWalletConnect 
+} from 'react-native-walletconnect';
+
 import {
   Button,
   SafeAreaView,
@@ -26,6 +31,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { getAppInfo } from 'react-native/Libraries/LogBox/Data/LogBoxData';
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -66,14 +72,22 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        {/* <Header /> */}
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-            <Button title='connect wallet'>
-            </Button>
-          <Section title="Step One">
+
+          <View
+            style={styles.walletConnect}
+          >
+              <WalletConnectProvider >
+                <WalletConnectExample />
+              </WalletConnectProvider>
+            
+          </View>
+
+          {/* <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
@@ -86,14 +100,57 @@ const App: () => Node = () => {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
+          <LearnMoreLinks /> */}
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
+const WalletConnectExample = () => {
+  const {
+    createSession,
+    killSession,
+    session,
+    signTransaction,
+  } = useWalletConnect();
+  const hasWallet = !!session.length;
+  return (
+    <>
+      {!hasWallet && (
+        <Button title="Connect" onPress={createSession} />
+      )}
+      {!!hasWallet && (
+        <Button
+          title="Sign Transaction"
+          onPress={() => signTransaction({
+            from: "0xbc28Ea04101F03aA7a94C1379bc3AB32E65e62d3",
+            to: "0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359",
+            data: "0x",
+            gasPrice: "0x02540be400",
+            gas: "0x9c40",
+            value: "0x00", 
+            nonce: "0x0114",
+          })}
+        />
+      )}
+      {!!hasWallet && (
+        <Button
+          title="Disconnect"
+          onPress={killSession}
+        />
+      )}
+    </>
+  );
+};
+
+
 const styles = StyleSheet.create({
+  walletConnect: {
+    paddingTop: 100,
+    width: window.width,
+    height: 700,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
